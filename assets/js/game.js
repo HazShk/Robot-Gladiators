e51d272f43bd4b170c9cae37e1777c409064eaa2
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-04 10:06:57 -0400

    Inital Round of Battle

---
+++ assets/js/game.js
@@ -0,0 +1,49 @@
+var playerName = window.prompt("What is your robot's name");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//Log Multiple values at once
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+  window.alert("Welcome to Robot Gladiators");
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      " . " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining."
+  );
+  //check enemy health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died. ");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+  }
+  playerHealth = playerHealth - enemyAttack;
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+  //check player health
+  if (playerHealth <= 0) {
+    window.alert((playerName = " has died "));
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
+fight();
