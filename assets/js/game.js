1d61eaefc3b681bad7ca8f2fa7d1c25cb0252c9d
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-05 08:38:01 -0400

    Used Math Object to generate random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,7 +36,7 @@ var fight = function (enemyName) {
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    enemyHealth = enemyHealth - Math.max(0, enemyHealth - playerAttack);
     console.log(
       playerName +
         " attacked " +
@@ -53,7 +53,7 @@ var fight = function (enemyName) {
       window.alert(enemyName + " has died!");
 
       // award player money for winning
-      playerMoney = playerMoney + 20;
+      playerMoney = Math.max(0, playerMoney + 20);
 
       // leave while() loop since enemy is dead
       break;
@@ -62,7 +62,8 @@ var fight = function (enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName +
         " attacked " +
@@ -95,7 +96,7 @@ var startGame = function () {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators1 Round" + (i + 1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       debugger;
       fight(pickedEnemyName);
 
@@ -177,6 +178,11 @@ var shop = function () {
       shop();
       break;
   }
+};
+//function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random * (max - min + 1)) + min;
+  return value;
 };
 // start the game
 startGame();
