cf64b7ab7d4e68c07aeb25509d0e0d51bbae7f59
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-05 15:09:26 -0400

    Player Name Prompt bug Completed

--- assets/js/game.js
+++ assets/js/game.js
@@ -195,9 +195,19 @@ var shop = function () {
 
 /* GAME INFORMATION / VARIABLES */
 
+// function to set name=vlookup
+var getPlayerName = function () {
+  var name = "";
+  // ADD LOOP HERE WITH PROMPT AND CONDITION
+  while (name === "" || name === null) {
+    name = window.prompt("What is your robot's name");
+  }
+  console.log("Your robot's name is" + name);
+  return name;
+};
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
